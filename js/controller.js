9f5d5ae4e4bc74079f21b5130f1412bd5e31fe20
acohrs <arcohrs@gmail.com>
2016-02-11 12:34:14 +0000

    js folder first commit

---
+++ js/controller.js
@@ -0,0 +1,10 @@
+angular.module('myControllers',[]);
+
+angular.module('myControllers').controller('MyController',function($scope)
+{
+    //controller logic here
+});
+
+angular.module('myControllers').controller('MyOtherController',function($scope) {
+    //my other controller logic here
+});
